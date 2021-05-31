f96e436171c863ed98467448cbc7ec22fe6c1dc1
Giacomo Bettini <bettoide@gmail.com>
2021-05-31 17:40:47 +0200

    Aggiunta funzione per calcolare quali sensori possono inviare dati a quali altri sensori. Definizione di un dizionario che contiene l'id di un sensore ed il numero dei suoi senders.

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 # Andrea Benini, Giacomo Bettini, Filippo Luppi
 import sys
 import random as rn
+import math
 from matplotlib import pyplot as plt
 
 max_x = 1200  # longitudine
 max_y = 800  # latitudine
+
+rn.seed(1625)  # Per la riproducibilità degli esempi
 
 
 class Sensor:
@@ -19,12 +22,12 @@ class Sensor:
 
 
 class Gateway:
-    def __init__(self, an_id):
+    def __init__(self, an_id, costo, capacita):
         self.id = an_id
         self.longitudine = 0
         self.latitudine = 0
-        self.costo = rn.randint(100, 1000)
-        self.capacita = rn.randint(50, 1000)
+        self.costo = costo
+        self.capacita = capacita
         self.capacita_elab_unit = "msg/s"
 
 
@@ -32,6 +35,34 @@ class Gateway:
 #    def __init__(self, num_sensori):
 #        self.num_sensori = num_sensori
 
+# Questa funzione, dato un sensore in input, trova tutti i sensori
+# che possono trasmettergli dati.
+def find_senders(a_sensor):
+    global sensors
+    sensors_without_self = sensors.copy()
+    # sensors_without_self.remove(a_sensor)
+    senders = []
+
+    for this_sens in sensors:
+        this_dist = distance(a_sensor, this_sens)
+        if this_dist <= this_sens.portata and this_dist != 0:
+            senders.append(this_sens)
+
+    return senders
+
+
+# Prende in input due sensori e restituisce
+# la loro distanza euclidea
+def distance(sens_one, sens_two):
+    x_0 = sens_one.longitudine
+    y_0 = sens_one.latitudine
+    x_1 = sens_two.longitudine
+    y_1 = sens_two.latitudine
+    return math.sqrt((y_0 - y_1) ** 2 + (x_0 - x_1) ** 2)
+
+
+sensors = []
+gateways = []
 
 if __name__ == '__main__':
     num_sensori = 50
@@ -39,10 +70,15 @@ if __name__ == '__main__':
     verbose = len(sys.argv) > 1 and "-v" in sys.argv
     quiet = len(sys.argv) > 1 and "-q" in sys.argv
 
-    sensors = []
-    gateways = []
     for i in range(num_sensori):
         sensors.append(Sensor(i))
+
+    # Definiamo il listino dei dispositivi
+    gateways.append(Gateway(1, 8, 8))
+    gateways.append(Gateway(2, 16, 16))
+    gateways.append(Gateway(3, 24, 24))
+    gateways.append(Gateway(4, 32, 32))
+    gateways.append(Gateway(5, 40, 40))
 
     x = []
     y = []
@@ -79,6 +115,18 @@ if __name__ == '__main__':
     for x_pos, y_pos, label in zip(x, y, labels):
         ax.annotate(label, xy=(x_pos, y_pos), xytext=(7, 0), textcoords='offset points',
                     ha='left', va='center')
+
+    print("Mostra i sender per tutti i sensori")
+    sens_dict = {}
+    for this_sens in sensors:
+        this_senders = find_senders(this_sens)
+        num_sensori = len(this_senders)
+        sens_dict[this_sens.id] = num_sensori
+        print("\nIl sensore " + str(this_sens.id) + " è nel raggio di " + str(num_sensori) + " altri sensori:")
+        for temp_sens in this_senders:
+            print(temp_sens.id)
+
+    print(sens_dict)
 
     ax.set_aspect('equal', anchor="C")
     ax.set_xbound(lower=0.0, upper=max_x)
